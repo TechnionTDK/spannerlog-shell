0097c9ebffc88fc9a3e281a08200387e0faf2763
yoavn <yoav491@gmail.com>
2017-08-16 12:41:10 +0300

    [installation,webserver]

--- installation/lib/ner.sql
+++ installation/lib/ner.sql
@@ -1,40 +1,75 @@
 CREATE FUNCTION ner (s text)
     RETURNS TABLE (entity_start int, entity_end int, category text)
 AS $$
-from pycorenlp import StanfordCoreNLP
-nlp = StanfordCoreNLP('http://localhost:9000')
-res = nlp.annotate(s, properties={
-    'annotators': 'ner',
-    'outputFormat': 'json'
-})  
+from stanfordcorenlp import StanfordCoreNLP
 
-if not isinstance(res, dict):
-    return
+nlp = StanfordCoreNLP('http://localhost')
 
-for sen in res["sentences"]:
-    begin = -1
-    end = -1
-    ner_prev = "O"
-    for token in sen["tokens"]:
-        ner_curr = str(token["ner"])
-        if ner_curr == ner_prev:
-            end = token["characterOffsetEnd"]
+entities = nlp.ner(s)
+
+entity_start = -1
+entity_end = -1
+prev_category = "O"
+for idx, (token, category, (m,n)) in enumerate(entities):
+    if category == prev_category:
+        entity_end = n
+    else:
+        if entity_start == -1 :
+            C = m
+            entity_end = n
+            prev_category = category
         else:
-            if begin == -1 :
-                begin = token["characterOffsetBegin"]
-                end = token["characterOffsetEnd"]
-                ner_prev = ner_curr
+            yield [entity_start + 1, entity_end + 1, prev_category]
+            if category != "O":
+                entity_start = m
+                entity_end = n
+                prev_category = category
             else:
-                yield [begin + 1, end + 1, ner_prev]
-                if ner_curr != "O":
-                    begin = token["characterOffsetBegin"]
-                    end = token["characterOffsetEnd"]
-                    ner_prev = ner_curr
-                else:
-                    begin = -1
-                    end = -1
-                    ner_prev = "O"
-    if begin != -1:
-        yield [begin + 1, end + 1, ner_prev]
+                entity_start = -1
+                entity_end = -1
+                ner_prev = "O"
+if entity_start != -1:
+    yield [entity_start + 1, entity_end + 1, prev_category]
 
 $$ LANGUAGE plpythonu;
+
+-- CREATE FUNCTION ner (s text)
+--     RETURNS TABLE (entity_start int, entity_end int, category text)
+-- AS $$
+-- from pycorenlp import StanfordCoreNLP
+-- nlp = StanfordCoreNLP('http://localhost:9000')
+-- res = nlp.annotate(s, properties={
+--     'annotators': 'ner',
+--     'outputFormat': 'json'
+-- })  
+
+-- if not isinstance(res, dict):
+--     return
+
+-- for sen in res["sentences"]:
+--     begin = -1
+--     end = -1
+--     ner_prev = "O"
+--     for token in sen["tokens"]:
+--         ner_curr = str(token["ner"])
+--         if ner_curr == ner_prev:
+--             end = token["characterOffsetEnd"]
+--         else:
+--             if begin == -1 :
+--                 begin = token["characterOffsetBegin"]
+--                 end = token["characterOffsetEnd"]
+--                 ner_prev = ner_curr
+--             else:
+--                 yield [begin + 1, end + 1, ner_prev]
+--                 if ner_curr != "O":
+--                     begin = token["characterOffsetBegin"]
+--                     end = token["characterOffsetEnd"]
+--                     ner_prev = ner_curr
+--                 else:
+--                     begin = -1
+--                     end = -1
+--                     ner_prev = "O"
+--     if begin != -1:
+--         yield [begin + 1, end + 1, ner_prev]
+
+-- $$ LANGUAGE plpythonu;
