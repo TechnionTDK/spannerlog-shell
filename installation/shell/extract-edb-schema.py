65ffbbfa063a0a00294679b81c8c3c6e276e34e5
yoavn <yoav491@gmail.com>
2017-09-10 09:51:10 +0300

    [installation]

--- installation/shell/extract-edb-schema.py
+++ installation/shell/extract-edb-schema.py
@@ -20,7 +20,11 @@ def main():
                 reader = csv.reader(infile, delimiter=delim)
                 
                 table_name = os.path.splitext(filename)[0]
-                db_schema[table_name] = extract_table_schema(next(reader))
+                headers = reader.next()
+                record = reader.next()
+                print(headers)
+                print(record)
+                db_schema[table_name] = extract_table_schema(headers, record)
 
             cmd = "tail -n +2 %s > %s.temp; mv %s.temp %s" % (filepath, filepath, filepath, filepath)
             df = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
@@ -30,11 +34,11 @@ def main():
     print(json.dumps(db_schema, sort_keys=True, indent=4))
 
 
-def extract_table_schema(row):
+def extract_table_schema(headers, record):
     cnt = 1
     table_schema = OrderedDict()
 
-    for val in row:
+    for i, val in enumerate(record):
         if val.isdigit() or (val.startswith('-') and val[1:].isdigit()):
             attr_type = 'int'
         elif val.replace('.','',1).isdigit() or (val.startswith('-') and val[1:].replace('.','',1).isdigit()):
@@ -43,7 +47,7 @@ def extract_table_schema(row):
             attr_type = "text"
 
         # table_schema["column" + str(cnt)] = attr_type
-        table_schema[val] = attr_type
+        table_schema[headers[i]] = attr_type
         cnt += 1
 
     return table_schema
