b44a751b0d0e65fe78b717628f3413a30219a565
yoavn <yoav491@gmail.com>
2017-09-11 17:23:16 +0300

    [installation]

---
+++ installation/lib/sentiment.sql
@@ -0,0 +1,17 @@
+CREATE FUNCTION sentiment (s text)
+    RETURNS TABLE (sentence_start int, sentence_end int, label text, value int)
+AS $$
+
+from stanfordcorenlpspl import StanfordCoreNLP
+import json
+
+nlp = StanfordCoreNLP('http://localhost')
+props={'annotators': 'sentiment'}
+annotations = json.loads(nlp.annotate(s, properties=props))
+for i, sen in enumerate(a["sentences"]):
+    if sen["tokens"]:
+    begin = sen["tokens"][0]["characterOffsetBegin"]
+    end = sen["tokens"][-1]["characterOffsetEnd"]
+    yield [begin, end, sen["sentiment"], sen["sentimentValue"]]
+
+$$ LANGUAGE plpythonu;
